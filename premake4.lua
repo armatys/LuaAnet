ebef48f3f3d8b0680a0638c5b3da02c901ed0dd6
mako52 <marmatys@sharpnose.eu>
2010-07-01 18:36:04 +0200

    Changed tabs to spaces

--- premake4.lua
+++ premake4.lua
@@ -1,23 +1,23 @@
 solution "Anet"
-	configurations { "release" }
-	flags { "ExtraWarnings", "NoFramePointer", "OptimizeSpeed" }
-	buildoptions { "-pedantic", "-std=c99" }
-	platforms { "native", "x32", "x64" }
+    configurations { "release" }
+    flags { "ExtraWarnings", "NoFramePointer", "OptimizeSpeed" }
+    buildoptions { "-pedantic", "-std=c99" }
+    platforms { "native", "x32", "x64" }
 
 project "Anet"
-	kind "SharedLib"
-	language "c"
-	location "build"
-	files { "src/*.c" }
-	targetprefix ""
-	targetname "anet"
-	
-	
-	configuration { "macosx" }
-		targetdir "build/macosx"
-		targetextension ".so"
-		linkoptions { "-single_module", "-undefined dynamic_lookup" }
-	
-	configuration { "linux" }
-		includedirs { "/usr/include/lua5.1" }
-		targetdir "build/linux"
+    kind "SharedLib"
+    language "c"
+    location "build"
+    files { "src/*.c" }
+    targetprefix ""
+    targetname "anet"
+    
+    
+    configuration { "macosx" }
+        targetdir "build/macosx"
+        targetextension ".so"
+        linkoptions { "-single_module", "-undefined dynamic_lookup" }
+    
+    configuration { "linux" }
+        includedirs { "/usr/include/lua5.1" }
+        targetdir "build/linux"
